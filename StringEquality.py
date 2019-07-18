7d13a7df817c2fa6236407ed71b9d11d54f21a6b
sanath777 <53030555+sanath777@users.noreply.github.com>
2019-07-18 09:50:18 +0530

    Add files via upload

---
+++ StringEquality.py
@@ -0,0 +1,12 @@
+n=1
+while(n==1):
+    n1=input()
+    n2=input()
+    if len(n1)>=50 or len(n2)>=50:
+        print("Re-enter Strings")
+    else:
+        break
+if n1==n2:
+    print("Equal")
+else:
+    print("Not Equal")
---
+++ perfectSquare.py
@@ -0,0 +1,7 @@
+import math
+n=int(input("Enter a number"))
+a=int(math.sqrt(n))
+if (a**2==n):
+    print("Perfect Square")
+else:
+    print("Not Perfect Square")
